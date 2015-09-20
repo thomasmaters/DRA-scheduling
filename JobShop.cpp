dff7b9b809e0ce62a5bb7a2c9b21a0c9d82e01db
thomasmaters <thomasmaters@hotmail.com>
2015-09-20 16:34:21 +0200

    Inlezen bestanden
    Working

--- JobShop.cpp
+++ JobShop.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "JobShop.hpp"
+#include "Job.hpp"
 #include <vector>
 #include <string>
 #include <iostream>
@@ -16,10 +17,16 @@ using namespace std;
 
 JobShop::JobShop()
 {
-	vector<pair<long,long> > Jobs;
+	vector<vector<pair<long,long>>> Jobs;
 	cout << "Input jobs file path." << endl;
 	readFile(readFromConsole());
 }
+
+JobShop::JobShop(const JobShop &JS)
+{
+	*this = JS;
+}
+
 
 string JobShop::readFromConsole() const
 {
@@ -27,20 +34,48 @@ string JobShop::readFromConsole() const
 	cin>>input_string;
 	return input_string;
 }
-
+//C:\Users\thomas\Desktop\test.txt
 string JobShop::readFile(const string fileName) const
 {
 	string line;
+	string output;
 	ifstream myfile (fileName);
+
 	if (myfile.is_open())
 	{
+		getline (myfile,line);
+
+
+	   // string test = "test replacing \"these characters\"";
+	   // regex reg("[^\\w]+");
+	    //test = regex_replace(test, reg, "_");
+	   // cout << test << endl;
 		while ( getline (myfile,line) )
 		{
+			regex reg("([0-9]*[0-9])");
+
+		    std::smatch match;
+		    while (regex_search (line,match,reg)) {
+		    	string match1(match[1]);
+		    	long int a = atoi(match1.c_str());
+		    	line = match.suffix().str();
+
+		    	regex_search (line,match,reg);
+		    	string match2(match[1]);
+		    	long int b = atoi(match2.c_str());
+		    	line = match.suffix().str();
+
+		    	vector<pair<long,long>> v;
+		    	v.push_back(make_pair(a,b));
+		    	Job j(v);
+		    	//cout << line << endl;
+		    }
 			cout << line << '\n';
 		}
 		myfile.close();
 	}else{
 		cout << "No file found, check your path and try again.." << endl;
+		readFile(readFromConsole());
 	}
 	return fileName;
 }
--- JobShop.hpp
+++ JobShop.hpp
@@ -8,17 +8,23 @@
 #ifndef JOBSHOP_HPP_
 #define JOBSHOP_HPP_
 #include <string>
+#include <vector>
 using namespace std;
 
 class JobShop
 {
 public:
 	JobShop();
+	JobShop(const JobShop &JS);
 
 	string readFromConsole() const;
 	string readFile(const string fileName) const;
 
 	virtual ~JobShop();
+private:
+	static long int job_count;
+	static long int machine_count;
+	vector<vector<pair<long,long>>> Jobs;
 };
 
 #endif /* JOBSHOP_HPP_ */
